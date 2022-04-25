d297423e2ddb5b698bf2148779ee81ecd41170e3
Michael Vrňata <michael.vrnata@arkance-systems.com>
2022-04-25 16:00:53 +0200

    Update

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,10 +1,32 @@
-﻿Console.WriteLine("Zadejte datum narození (ve formátu 1.1.2000)");
+﻿//Console.WriteLine("Zadejte datum narození (ve formátu 1.1.2000)");
 
-DateTime birthday = DateTime.Parse(Console.ReadLine());
+//DateTime birthday = DateTime.Parse(Console.ReadLine());
 
-CalculateAgeFromBirthday(birthday);
+//CalculateAgeFromBirthday(birthday);
 
-void CalculateAgeFromBirthday(DateTime birthday)
+//void CalculateAgeFromBirthday(DateTime birthday)
+//{
+//    Console.WriteLine(Math.Floor(DateTime.Today.Subtract(birthday).TotalDays / 365));
+//}
+
+Console.WriteLine("Zadejte číslo dne");
+
+int dayNumber = int.Parse(Console.ReadLine());
+
+GetDayFromNumber(dayNumber);
+
+void GetDayFromNumber(int dayNumber)
 {
-    Console.WriteLine(Math.Floor(DateTime.Today.Subtract(birthday).TotalDays / 365));
+    Console.WriteLine((DaysOftheWeek)dayNumber);
+}
+
+enum DaysOftheWeek
+{
+    Pondělí = 1,
+    Úterý = 2,
+    Středa = 3,
+    Čtvrtek = 4,
+    Pátek = 5,
+    Sobota = 6,
+    Neděle = 7
 }
