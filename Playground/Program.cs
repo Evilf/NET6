0c2ec2238c1ab68f67a3ffc1ee9ce7ad7c65517f
Michael Vrňata <michael.vrnata@arkance-systems.com>
2022-04-25 12:16:53 +0200

    Update

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,16 +1,29 @@
 ﻿
-Console.WriteLine("Napiš první číslo a zmáčkni enter");
-int cislo1 = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Napiš druhé číslo a zmáčkni enter");
-int cislo2 = int.Parse(Console.ReadLine());
+while (true)
+{
+    Console.WriteLine("Napiš první číslo a zmáčkni enter");
+    int cislo1 = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Napiš třetí číslo a zmáčkni enter");
-int cislo3 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Napiš druhé číslo a zmáčkni enter");
+    int cislo2 = int.Parse(Console.ReadLine());
 
-if (cislo1 > cislo2 && cislo1 > cislo3)
-    Console.WriteLine($"Největší je číslo: {cislo1}");
-else if (cislo2 > cislo1 && cislo2 > cislo3)
-    Console.WriteLine($"Největší je číslo: {cislo2}");
-else
-    Console.WriteLine($"Největší je číslo: {cislo3}");
+    Console.WriteLine("Napiš třetí číslo a zmáčkni enter");
+    int cislo3 = int.Parse(Console.ReadLine());
+
+    if (cislo1 > cislo2)
+    {
+        if (cislo1 > cislo3)
+        {
+            Console.WriteLine($"Největší je číslo: {cislo1}");
+        }
+    }
+    else if (cislo2 > cislo3)
+    {
+        Console.WriteLine($"Největší je číslo: {cislo2}");
+    }
+    else
+    {
+        Console.WriteLine($"Největší je číslo: {cislo3}");
+    }
+}
