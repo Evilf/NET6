b92cd2ddb0ddf24ece10d77ea6d8638d345dab19
Michael Vrňata <michael.vrnata@arkance-systems.com>
2022-04-25 12:03:52 +0200

    Update

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,8 +1,16 @@
 ﻿
-Console.WriteLine("Napiš teplotu v celsiu a zmáčkni enter");
+Console.WriteLine("Napiš první číslo a zmáčkni enter");
+int cislo1 = int.Parse(Console.ReadLine());
 
-string? vstupCelsius = Console.ReadLine();
-int convertedToFarenheit = ((int)Math.Round((int.Parse(vstupCelsius) * 1.8) + 32));
+Console.WriteLine("Napiš druhé číslo a zmáčkni enter");
+int cislo2 = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Napiš třetí číslo a zmáčkni enter");
+int cislo3 = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Výsledek: {convertedToFarenheit}");
+if (cislo1 > cislo2 && cislo1 > cislo3)
+    Console.WriteLine($"Největší je číslo: {cislo1}");
+else if (cislo2 > cislo1 && cislo2 > cislo3)
+    Console.WriteLine($"Největší je číslo: {cislo2}");
+else
+    Console.WriteLine($"Největší je číslo: {cislo3}");
