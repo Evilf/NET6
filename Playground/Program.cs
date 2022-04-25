377d260f09ca9e77425321e544db6a90866e0306
Michael Vrňata <michael.vrnata@arkance-systems.com>
2022-04-25 14:32:22 +0200

    Update

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,9 +1,15 @@
-﻿int cislo = int.Parse(Console.ReadLine());
-int vysledek = 0;
+﻿Console.WriteLine("Zadejte počet řádků.");
 
-for (int i = 1; i <= cislo; i++)
+int rowCount = int.Parse(Console.ReadLine());
+string result = "";
+
+for (int i = 1; i <= rowCount; i++)
 {
-    vysledek += i;
+    for(int j = 1; j <= i; j++)
+    {
+        result += i.ToString();
+    }
+    result += "\n";
 }
 
-Console.WriteLine($"Výsledek je: {vysledek}");
+Console.WriteLine($"Výsledek je: {result}");
