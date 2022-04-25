801ef5f00e7c38a72d8d951e3477899a7fd1bb27
Michael Vrňata <michael.vrnata@arkance-systems.com>
2022-04-25 13:59:06 +0200

    Update

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -4,24 +4,44 @@ do
     Console.Clear();
 
     Console.WriteLine("Napiš číslo dne a zmáčkni enter");
-    int cisloDne = int.Parse(Console.ReadLine());
 
-    if (cisloDne == 1)
-        Console.WriteLine("1 = Pondělí");
-    else if (cisloDne == 2)
-        Console.WriteLine("2 = Úterý");
-    else if (cisloDne == 3)
-        Console.WriteLine("3 = Středa");
-    else if (cisloDne == 4)
-        Console.WriteLine("4 = Čtvrtek");
-    else if (cisloDne == 5)
-        Console.WriteLine("5 = Pátek");
-    else if (cisloDne == 6)
-        Console.WriteLine("6 = Sobota");
-    else if(cisloDne == 7)
-        Console.WriteLine("7 = Neděle");
+    int cisloDne = int.Parse(Console.ReadLine());
+    bool jeVikend = false;
+
+    switch (cisloDne)
+    {
+        case 1:
+            Console.WriteLine("1 = Ponděl");
+            break;
+        case 2:
+            Console.WriteLine("2 = Úterý");
+            break;
+        case 3:
+            Console.WriteLine("3 = Středa");
+            break;
+        case 4:
+            Console.WriteLine("4 = Čtvrtek");
+            break;
+        case 5:
+            Console.WriteLine("5 = Pátek");
+            break;
+        case 6:
+            Console.WriteLine("6 = Sobota");
+            jeVikend = true;
+            break;
+        case 7:
+            Console.WriteLine("7 = Neděle");
+            jeVikend = true;
+            break;
+        default:
+            Console.WriteLine("Špatné číslo!");
+            break;
+    }
+
+    if (jeVikend)
+        Console.WriteLine("Je víkend");
     else
-        Console.WriteLine("Špatné číslo!");
+        Console.WriteLine("Není víkend");
 
     Console.WriteLine("Chcete ukončit? y/n");
 }
