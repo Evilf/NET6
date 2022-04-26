1ea89a0110509a19fde3fc7ce632375f5e9acaf8
Michael Vrňata <michael.vrnata@arkance-systems.com>
2022-04-26 14:05:20 +0200

    New Class

---
+++ Playground/Model/Auto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.Model
+{
+    public class Auto
+    {
+        public string SPZ { get; set; }
+        public Color Color { get; set; }
+        public double KilometersDriven { get; set; }
+        public string CarBrand { get; set; }
+
+        public Auto()
+        {
+
+        }
+
+        public Auto(string sPZ, Color color, double kilometersDriven, string carBrand)
+        {
+            SPZ = sPZ;
+            Color = color; 
+            KilometersDriven = kilometersDriven;
+            CarBrand = carBrand;
+        }
+
+    }
+}
