0974d9bafca81d8cc2d4feb11954973a9139496e
Michael Vrňata <michael.vrnata@arkance-systems.com>
2022-04-26 10:14:40 +0200

    New Class

---
+++ Playground/Model/XY.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.Model
+{
+    public class XY
+    {
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public XY(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,23 +1,4 @@
-﻿string numbers = "";
-int result = 0;
-string input = "";
+﻿using Playground.Model;
 
-do
-{
-    Console.WriteLine("Zadejte celé číslo nebo Q pro konec");
-    input = Console.ReadLine();
-
-    if (numbers == "" && input.All(char.IsDigit))
-        numbers += input;
-    else if (input.All(char.IsDigit))
-        numbers += ", " + input;
-
-    if (input.All(char.IsDigit))
-        result += int.Parse(input);
-}
-while (input != "q" && input != "Q");
-
-if (numbers != "")
-    Console.WriteLine($"Součet čísel: {numbers} je {result}");
-else
-    Console.WriteLine("Nebyla zadána žádná čísla");
+XY point1 = new XY(5, 4);
+XY point2 = new XY(5, 8);
